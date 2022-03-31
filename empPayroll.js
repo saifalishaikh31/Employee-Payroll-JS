ced913b3842f6df4256f5c36d1805c85eac9bfce
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-31 13:08:03 +0530

    [Saif Ali] Add : Ability to extend Employee Payroll Data to store gender and start date Added

--- empPayroll.js
+++ empPayroll.js
@@ -1,24 +1,33 @@
 class EmployeePayroll
 {
     id;
+    name;
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary)
+    constructor(...params)
     {
-        this.id  = id;
-        this.name = name;
-        this.salary = salary;
+        this.id  = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     get name(){ return this._name; }
     set name(name){ this._name = name; }
 
     toString(){
-        return "id : " + this.id + ", name : " + this.name+", salary : " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id : " + this.id + ", name : " + this.name+", salary : " + this.salary + ", Gender : " + this.gender + ", StartDate : "+ empDate;
     }
 }
 
-let employeePayroll = new EmployeePayroll(1,"Lawerence",50000);
+let employeePayroll = new EmployeePayroll(1,"Lawerence",50000,"M",new Date());
 console.log(employeePayroll.toString());
 employeePayroll.name = "john";
 console.log(employeePayroll.toString());
+let newEmployeePayroll = new EmployeePayroll(2,"Terissa",30000,"F",new Date());
+console.log(newEmployeePayroll.toString());
